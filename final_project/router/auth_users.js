1071d6e27a8ab3d455984f3e247b7e36e2d4f42d
Abel Tesfa <abeltesfa198@gmail.com>
2024-08-30 15:11:56 +0300

    auth_user.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,45 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  const user = users.find((user) => user.username === username);
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+  if (user) {
+    return true;
+  }
+  return false;
+};
+
+const authenticatedUser = (username, password) => {
+  const user = users.find((user) => user.username === username);
+  if (username && user.password === password) {
+    return true;
+  }
+  return false;
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(username)) {
+    if (authenticatedUser(username, password)) {
+      const token = jwt.sign({ username }, "access", { expiresIn: "30s" });
+      return res.json({ token });
+    }
+  }
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
